8976bf06c0e9b6e448c59588dc1ec17a040cd4d3
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-05-17 23:16:37 +0100

    Cleaning out dark mode comments

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -190,13 +190,7 @@ FOR DARK MODE
 --color-grey-50: #111827;
 --color-grey-100: #1f2937;
 --color-grey-200: #374151;
---color-grey-300: #4b5563;
---color-grey-400: #6b7280;
---color-grey-500: #9ca3af;
---color-grey-600: #d1d5db;
---color-grey-700: #e5e7eb;
---color-grey-800: #f3f4f6;
---color-grey-900: #f9fafb;
+
 
 */
 `;
